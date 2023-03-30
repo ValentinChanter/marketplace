65f812b99007417b1038aa8b33c4975e69c5f53f
ValentinChanter <valentinsing19@gmail.com>
2023-03-30 22:51:31 +0200

    Added lateral menu icon, account icon, cart icon. Made a better search bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,39 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './navbar.module.css'
 
 export default function Navbar({pageName}: {pageName: String}) {
     return (
         <div className={styles.container}>
-            <div>
-                
+            <div className={styles.leftLogosContainer}>
+                <div className={styles.menuLogo}>
+                    <Image src="/menu.png" fill sizes='100vw' alt="Menu latéral" />
+                </div>
+
+                <div className={styles.logo}>
+                    <Link href="/">
+                        <Image src="/next.svg" fill sizes='100vw' alt="Logo" />
+                    </Link>
+                </div>
             </div>
-            <div className={styles.logo}>
-                <Image src="/next.svg" fill sizes='100vw' alt="Logo" />
-            </div>
-            <form className={styles.searchForm}>
+
+            <form action="/api/searchForm" method="post" className={styles.searchForm}>
                 <input type="search" placeholder="Rechercher un produit" className={styles.search}/>
-                <button type="submit" style={{ margin: 'auto', marginLeft: "0", height: '70%' }}>
-                    <div className={styles.searchButton}>
-                        <Image src="/search.png" fill sizes='100vw' alt="Rechercher" />
+                <button type="submit" className={styles.searchButton}>
+                    <div className={styles.searchButtonDiv}>
+                        <Image src="/searchWhite.png" fill sizes='100vw' alt="Rechercher" />
                     </div>
                 </button>
             </form>
+
+            <div className={styles.rightLogosContainer}>
+                <div className={styles.squareLogo}>
+                    <Image src="/user.png" fill sizes='100vw' alt="Profil" />
+                </div>
+                <div className={styles.squareLogo}>
+                    <Image src="/cart.png" fill sizes='100vw' alt="Panier" />
+                </div>
+            </div>
         </div>
     )
 }
