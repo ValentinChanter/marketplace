2e07bd883ff1c62669e3928ba61a00b3d9ee865b
ValentinChanter <valentinsing19@gmail.com>
2023-04-17 14:23:05 +0200

    Added cookie password for iron-session

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SECRET_COOKIE_PASSWORD: "E9Rd++8yZG~>a>jtM*U!)NTtP>@}XANB",
+  },
 }
 
 module.exports = nextConfig
