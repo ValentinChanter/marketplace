dab6367498e9136e7630d875834c656258da23af
ValentinChanter <singvalent@cy-tech.fr>
2023-04-18 22:24:40 +0200

    Updated user icon dropdown menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from './navbar.module.css'
 import { User } from '@/lib/session';
+import fetchJson, { FetchError } from "@/lib/fetchJson";
 
 export default function Navbar({pageName, user}: {pageName:string, user:User}) {
+    console.log(user)
     return (
         <>
             <div className={styles.container}>
@@ -30,31 +32,33 @@ export default function Navbar({pageName, user}: {pageName:string, user:User}) {
 
                 <div className={styles.rightLogosContainer}>
                     {user !== null ? (
-                        <div className={`relative group ${styles.squareLogo}`}>
-                            <Link href="/commandes">
-                                <Image src="/user.png" fill sizes='100vw' alt="Profil"/>
-                            </Link>
-                            <ul className="absolute z-10 hidden group-hover:block mt-1 py-2 w-48 bg-white rounded-md shadow-lg right-0.5">
-                                <li>
-                                    <Link href="/commandes">
-                                        <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 transition duration-150 ease-in-out">
-                                            Mes commandes
+                        <div className='relative group'>
+                            <div className={styles.userLogo}>
+                                <Link href="/commandes">
+                                    <Image src="/user.png" fill sizes='100vw' alt="Profil"/>
+                                </Link>
+                                <ul className="absolute z-10 hidden group-hover:block mt-1 py-2 w-48 bg-white rounded-b-md shadow-lg top-12 right-0">
+                                    <li>
+                                        <Link href="/commandes">
+                                            <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 transition duration-150 ease-in-out">
+                                                Mes commandes
+                                            </p>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/abonnement">
+                                            <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 transition duration-150 ease-in-out">
+                                                Abonnement
+                                            </p>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <p onClick={() => logout()} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 transition duration-150 ease-in-out cursor-pointer">
+                                            Se déconnecter
                                         </p>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/abonnement">
-                                        <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 transition duration-150 ease-in-out">
-                                            Abonnement
-                                        </p>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 transition duration-150 ease-in-out">
-                                        Se déconnecter
-                                    </p>
-                                </li>
-                            </ul>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     ) : (
                         <div className={styles.squareLogo}>
@@ -70,4 +74,20 @@ export default function Navbar({pageName, user}: {pageName:string, user:User}) {
             </div>
         </>
     )
+}
+
+async function logout() {
+    // TODO: Rediriger quand l'utilisateur est déconnecté
+    try {
+        await fetchJson("/api/logout", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        })
+    } catch (error) {
+      if (error instanceof FetchError) {
+        console.error(error.data.message);
+      } else {
+        console.error("An unexpected error happened:", error);
+      }
+    }
 }
