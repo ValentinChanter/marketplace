9b087e801377cca380ca7e7d0c114657cb06e070
ValentinChanter <singvalent@cy-tech.fr>
2023-04-19 23:44:23 +0200

    Renamed form component and changed username to email

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,6 +1,6 @@
 import { FormEvent } from 'react'
 
-export default function Form({
+export default function LoginForm({
   errorMessage,
   onSubmit,
 }: {
@@ -11,11 +11,11 @@ export default function Form({
     <form onSubmit={onSubmit} className="flex flex-col">
 
       <label className="font-semibold">Adresse mail</label>
-      <input type="text" name="username" required className='p-2 mt-1 mb-4 border-solid border rounded border-gray-600 focus:border-gray-700' />
+      <input type="email" name="email" required className='p-2 mt-1 mb-4 border-solid border rounded border-gray-600 focus:border-gray-700' />
       <label className="font-semibold">Mot de passe</label>
       <input type="password" name="password" required className='p-2 mt-1 mb-4 border-solid border rounded border-gray-600' />
 
-      <button type="submit" className='text-white border-solid border-3 border-gray-800 rounded bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 py-2 my-1'>Connexion</button>
+      <button type="submit" className='text-white border-solid border-3 border-gray-800 rounded bg-teal-500 hover:bg-teal-600 focus:ring-2 focus:ring-blue-300 py-2 my-1'>Connexion</button>
 
       {errorMessage && <p className="text-red-600 mt-4 mb-2">{errorMessage}</p>}
     </form>
