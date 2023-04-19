8fd91a7b245686fda00e7f00fbfde7cd39956c82
ValentinChanter <valentinsing19@gmail.com>
2023-04-19 10:47:41 +0200

    Changed js files to ts

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,8 +1,10 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import { sessionOptions } from "@/lib/session";
 import prisma from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { User } from "./user";
 
-export default withIronSessionApiRoute(async (req, res) => {
+export default withIronSessionApiRoute(async (req: NextApiRequest, res: NextApiResponse) => {
   const { username, password } = await req.body;
 
   try {
@@ -16,12 +18,12 @@ export default withIronSessionApiRoute(async (req, res) => {
     if (!userInfos[0] || userInfos[1]) res.status(500).json({ message: "Adresse mail ou mot de passe invalide(s). Veuillez réessayer."});
     else {
       const userInfo = userInfos[0];
-      const user = { id: userInfo.id, status: userInfo.status };
+      const user = { id: userInfo.id, status: userInfo.status, isSubscribed: userInfo.subscribed, isLoggedIn: true } as User;
       req.session.user = user;
       await req.session.save();
       res.json(user);
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: (error as Error).message });
   }
 }, sessionOptions);
