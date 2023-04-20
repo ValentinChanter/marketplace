6a7dbd3f0f1cdc455c69eda57417a3f163e85396
ValentinChanter <valentinsing19@gmail.com>
2023-04-21 00:31:56 +0200

    Used redirection lib and simplified redirection

--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -2,20 +2,18 @@ import { useEffect } from "react";
 import Router from "next/router";
 import useSWR from "swr";
 import { User } from "@/pages/api/user";
+import redirection from "./redirection";
 
 export default function useUser({
-  redirectTo = "",
   redirectIfFound = false,
 } = {}) {
   const { data: user, mutate: mutateUser } = useSWR<User>("/api/user");
 
   useEffect(() => {
-    // if no redirect needed, just return (example: already on /dashboard)
-    // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
-    if (!redirectTo || !user) return;
+    if (!user || !redirectIfFound) return;
 
-    if (redirectIfFound && user?.isLoggedIn) Router.push(redirectTo);
-  }, [user, redirectIfFound, redirectTo]);
+    if (user?.isLoggedIn) Router.push(redirection(user).path);
+  }, [user, redirectIfFound]);
 
   return { user, mutateUser };
 }
