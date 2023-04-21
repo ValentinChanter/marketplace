fb57d6e55100f20cc97631097bd79d924d1b097c
ValentinChanter <valentinsing19@gmail.com>
2023-04-21 15:45:24 +0200

    Added an unauthorized checking component

--- src/pages/delivery.tsx
+++ src/pages/delivery.tsx
@@ -1,38 +1,29 @@
-import checkUserStatus from "@/lib/checkUserStatus";
 import { User } from "./api/user";
 import { GetServerSideProps } from 'next'
 import { withIronSessionSsr } from "iron-session/next";
 import { sessionOptions } from '@/lib/session';
 import Layout from "@/components/layout";
+import StatusLockedPage from "@/components/statusLockedPage";
 import fetchJson from "@/lib/fetchJson";
 
 export default function Delivery({user}: {user:User}) {
-    if(checkUserStatus(user, "DELIVERY")) {
-		const body = {
-			id: user.id,
-		};
-	
-		fetchJson("/api/delivery", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(body),
-		}).then(res => console.log(res))
-	
-		return(
-			<>
-				<Layout pageName={"Mes livraisons"} user={user}>
-					test
-				</Layout>
-			</>
-		)
-	} else {
-		return(
-			<>
-				<Layout pageName={"Non autorisé"} user={user}>
-				</Layout>
-			</>
-		)
-	}
+	return (
+		<StatusLockedPage user={user} status="DELIVERY" f={() => {
+			const body = {
+				id: user.id,
+			};
+		
+			fetchJson("/api/delivery", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(body),
+			}).then(res => console.log(res))
+		}}>
+			<Layout pageName={"Mes livraisons"} user={user}>
+				test
+			</Layout>
+		</StatusLockedPage>
+	)
 }
 
 export const getServerSideProps: GetServerSideProps = withIronSessionSsr(
