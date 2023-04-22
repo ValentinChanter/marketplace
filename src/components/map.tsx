f7ff39bfdb25d4d94d29338ead9ecff694f3aa24
ValentinChanter <valentinsing19@gmail.com>
2023-04-22 22:56:39 +0200

    Added leaflet map and routing on delivery page

---
+++ src/components/map.tsx
@@ -0,0 +1,74 @@
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.js';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'
+import L from "leaflet";
+import "leaflet-routing-machine";
+import { LatLngExpression } from 'leaflet';
+import { useEffect } from 'react';
+import 'leaflet-defaulticon-compatibility';
+
+export function ChangeView({ coords }: { coords: LatLngExpression }) {
+    const map = useMap();
+    map.setView(coords, 12);
+    return null;
+}
+
+export function Routing({addresses}: {addresses: any}) {
+    const map = useMap();
+  
+    useEffect(() => {
+        if (!map) return;
+
+        const waypoints = addresses.map((address:any) => L.latLng(address.lat, address.lng));
+        waypoints.push(waypoints[0]);
+
+        const routingControl = L.Routing.control({
+            waypoints,
+            fitSelectedRoutes: true,
+            draggableWaypoints: false,
+            routeWhileDragging: false,
+            createMarker: function() { return null; },
+            lineOptions: {
+                addWaypoints: false,
+                styles: [{ color: '#ff0000', weight: 2 }],
+                extendToWaypoints: false,
+                missingRouteTolerance: 0
+            }
+        }).addTo(map);
+  
+      return () => map.removeControl(routingControl);
+    }, [map]);
+  
+    return null;
+}
+
+const Map = ({addresses}: {addresses: any}) => {
+    const geoData = { lat: 49.0338636, lng: 2.0720143 };
+
+    const center = [geoData.lat, geoData.lng] as LatLngExpression;
+
+    return (
+        <MapContainer zoom={3} style={{ height: '80vh' }}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            {addresses ? addresses.map((a:any) => {
+                return <Marker position={[a.lat, a.lng]} key={String(a.lat) + String(a.lng)} draggable={false} />
+            }) : (
+                <Marker position={center} />
+            )}
+
+            {addresses ? (
+                <Routing addresses={addresses}/>
+            ) : (
+                <></>
+            )}
+        </MapContainer>
+    )
+};
+
+export default Map;
