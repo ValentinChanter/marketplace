e9a33bb384034b3c3d4ec754c1d2cc5a91bef754
ValentinChanter <valentinsing19@gmail.com>
2023-04-23 16:34:03 +0200

    Changed texts to include Nook

--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -8,10 +8,10 @@ export default function legal({user}: {user:User}) {
 	return (
 		<Layout pageName="Mentions légales" user={user}>
 			<div className='flex flex-col'>
-                <p className='text-justify mb-2'>Ce site web est la propriété de Adam, Amandine, Line et Valentin.</p>
+                <p className='text-justify mb-2'>La marketplace Nook est la propriété de Adam, Amandine, Line et Valentin.</p>
                 <p className='text-justify mb-2'>Siège social : Avenue du Parc, 95000 Cergy</p>
                 <p className='text-justify mb-2'>Téléphone : 06 00 00 00 00</p>
-                <p className='text-justify mb-2'>E-mail : marketplace@cy-tech.fr</p>
+                <p className='text-justify mb-2'>E-mail : nook@cy-tech.fr</p>
 
                 <p className='text-lg font-semibold mt-8'>Limitation de responsabilité</p>
                 <p className='text-justify mb-2'>Le propriétaire de ce site web ne saurait être tenu responsable des dommages directs ou indirects, quels qu'ils soient, résultant de l'accès à ce site web ou de son utilisation, y compris notamment toute perte d'exploitation, de profit, de revenu, de contrat, de données ou toute autre perte de nature financière ou commerciale.</p>
